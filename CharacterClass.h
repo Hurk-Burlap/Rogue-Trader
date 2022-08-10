8a96f9392890e715b7482443b7ece41ea71858c3
Logan Weatherall <biglolostyle@gmail.com>
2022-08-10 15:12:20 -0700

    Initial Solution and Code Files Add starting of character class

---
+++ CharacterClass.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "String"
+using namespace std;
+
+class Character
+{
+protected:
+    //Basic universal character info
+    string name;
+    string career;
+    string rank;
+
+    //Universal Characteristics
+    int weaponSkill;
+    int ballisticSkill;
+    int strength;
+    int toughness;
+    int agility;
+    int intelligence;
+    int perception;
+    int willPower;
+    int fellowship;
+
+    //Universal stats
+    int wounds;
+    int insanity;
+    int corruption;
+    int hArmor;
+    int tArmor;
+    int rArmArmor;
+};
